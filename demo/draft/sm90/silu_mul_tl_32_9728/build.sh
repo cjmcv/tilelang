9331983809e0dd4fbc64951e1ac9c0b172842473
cjmcv <398342899@qq.com>
2026-04-14 15:57:32 +0800

    .

---
+++ demo/draft/sm90/silu_mul_tl_32_9728/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# naive_gemm/gemm_multistage.cu
+# streamk_gemm/gemm_streamk.cu  
+# gemm_simple.cu
+# -L../build/lib -ltilelang_module -ltilelang \
+# -L../build/tvm -ltvm_ffi \
+
+rm a.out
+nvcc -gencode arch=compute_90,code=sm_90a \
+     -I../../../../3rdparty/cutlass/include \
+     -I../../../../src/ \
+     host.cu main.cu
+
+echo "Compile Done!"
+
+./a.out
