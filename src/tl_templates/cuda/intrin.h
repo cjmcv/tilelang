9e31905f8b2790211438426bdc925070bb914620
cjmcv <398342899@qq.com>
2026-04-29 10:43:54 +0800

    setmaxnreg仅在sm90a上支持, h20(sm90)也不支持

--- src/tl_templates/cuda/intrin.h
+++ src/tl_templates/cuda/intrin.h
@@ -122,13 +122,13 @@ template <int thread_extent> TL_DEVICE bool tl_shuffle_elect() {
 }
 
 template <uint32_t RegCount> TL_DEVICE void warpgroup_reg_alloc() {
-#if __CUDA_ARCH_LIST__ == 900
+#if defined(__CUDA_ARCH_SPECIFIC__) && __CUDA_ARCH__  == 900
   asm volatile("setmaxnreg.inc.sync.aligned.u32 %0;\n" : : "n"(RegCount));
 #endif
 }
 
 template <uint32_t RegCount> TL_DEVICE void warpgroup_reg_dealloc() {
-#if __CUDA_ARCH_LIST__ == 900
+#if defined(__CUDA_ARCH_SPECIFIC__) && __CUDA_ARCH__  == 900
   asm volatile("setmaxnreg.dec.sync.aligned.u32 %0;\n" : : "n"(RegCount));
 #endif
 }
