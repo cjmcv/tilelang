986c3140423038386db86ea4f45109fa6f434225
cjmcv <398342899@qq.com>
2026-05-08 17:15:57 +0800

    .

--- src/megakernel/persistent_kernel/tasks/sm_120/linear.cuh
+++ src/megakernel/persistent_kernel/tasks/sm_120/linear.cuh
@@ -8,7 +8,6 @@
 #include "m1/linear_gemm_tl_1_6144_1024.cuh"
 #include "m1/linear_gemm_add_tl_1_1024_3072.cuh"
 #endif // ENABLE_PREFETCH
-#else
 // attn
 #include "m1/linear_gemm_tl_1_4096_1024.cuh"
 #include "m1/linear_gemm_add_tl_1_1024_2048.cuh"
